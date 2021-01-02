310d12e595800d41c38643432cccfbdf3eb5b235
mikhlasnr <minrnullpo@gmail.com>
2021-01-03 00:10:19 +0700

    add background features and ixing bug background features

--- src/components/featuresSection/FeaturesSection.jsx
+++ src/components/featuresSection/FeaturesSection.jsx
@@ -1,10 +1,14 @@
+import { Container } from "@material-ui/core";
 import React from "react";
 import "./FeaturesSection.scss";
-
 const FeaturesSection = () => {
   return (
     <section id="features">
-      <h1>Its Features</h1>
+      <div className="backgroun-features">
+        <Container>
+          <h1>Its Features</h1>
+        </Container>
+      </div>
     </section>
   );
 };
